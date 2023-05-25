9a704c6b36c7734626aaf7b87b99e9dc90622b3e
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-05-25 13:58:42 +0700

    Menambahkan role Rekam Medis

---
+++ application/views/rekam_medis/index.php
@@ -0,0 +1,133 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active"><?= $title; ?></li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <div class="col-12">
+        <?php if (validation_errors()) : ?>
+          <div class="alert alert-danger" role="alert">
+            <?= validation_errors(); ?>
+          </div>
+        <?php endif; ?>
+
+        <?= $this->session->flashdata('message'); ?>
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div id="example2_filter" class="dataTables_filter">
+              <label class="col-md-6 d-flex float-right">Search:
+                <input type="search" class="form-control form-control-sm" placeholder="Search for..." aria-controls="example2">
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <table id="example2" class="table table-bordered table-hover text-center">
+              <thead>
+                <tr class="table-active">
+                  <th>No</th>
+                  <th>No Rg</th>
+                  <th>No MR</th>
+                  <th>Nama Pasien</th>
+                  <th>Alamat</th>
+                  <th>Status</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php $i = 1; ?>
+                <?php foreach ($Kebidanan as $kbd) : ?>
+                  <tr>
+                    <th scope="row"><?= $i; ?></th>
+                    <td><?= $kbd['no_rg']; ?></td>
+                    <td><?= $kbd['no_rm']; ?></td>
+                    <td><?= $kbd['nama_pasien']; ?></td>
+                    <td><?= $kbd['alamat']; ?></td>
+                    <td><?= $kbd['status']; ?></td>
+                    <td>
+                      <button type="button" class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-pen-to-square"></i> Entry</button>
+                      <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-clock-rotate-left"></i> History</button>
+                      <button type="button" class="btn btn-outline-danger btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-print"></i> Print</button>
+                    </td>
+                  </tr>
+                  <?php $i++; ?>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+</section>
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal -->
+<div class="modal fade" id="newSubMenuModal" tabindex="-1" aria-labelledby="newSubMenuModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="newSubMenuModalLabel">Add New Sub Menu</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="<?= base_url('menu/submenu'); ?>" method="post">
+        <div class="modal-body">
+          <div class="form-group">
+            <input type="title" class="form-control" id="title" name='title' placeholder="Submenu title">
+          </div>
+          <div class="form-group">
+            <select name="menu_id" id="menu_id" class="form_control">
+              <option value="">Select Menu</option>
+              <?php foreach ($menu as $m) : ?>
+                <option value="<?= $m['id']; ?>"><?= $m['menu']; ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <input type="title" class="form-control" id="url" name='url' placeholder="Submenu url">
+          </div>
+          <div class="form-group">
+            <input type="title" class="form-control" id="icon" name='icon' placeholder="Submenu icon">
+          </div>
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" value="1" name='is_active' id="is_active" checked>
+              <label for="is_active" class="form-check-label">Active?</label>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
