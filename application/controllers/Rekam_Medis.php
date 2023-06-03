ffada0d1bd75a63737a001b17c0d863dd9c2796f
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-03 11:24:15 +0700

    Membuat modal create pada master data pasien

--- application/controllers/Rekam_Medis.php
+++ application/controllers/Rekam_Medis.php
@@ -25,4 +25,37 @@ class Rekam_Medis extends CI_Controller
         $this->load->view('rekam_medis/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function create()
+    {
+        $data['title'] = 'Tambah Data Pasien*';
+        $this->form_validation->set_rules('id_pasien', 'ID Pasien', 'required|trim|is_unique[tb_pasien.id_pasien]', [
+            'is_unique' => 'Patient ID already registered!'
+        ]);
+        $this->form_validation->set_rules('no_rm', 'No RM', 'required|trim|is_unique[tb_pasien.no_rm]', [
+            'is_unique' => 'RM number already registered!'
+        ]);
+        $this->form_validation->set_rules('nama_pasien', 'Nama Pasien', 'required|trim');
+        $this->form_validation->set_rules('tgl_lahir', 'Tanggal Lahir', 'required|trim');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required|trim');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Tambah Data Pasien*';
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            // $this->load->view('templates/topbar', $data);
+            $this->load->view('rekam_medis/create', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->db->insert('tb_pasien', [
+                'id_pasien' => htmlspecialchars($this->input->post('id_pasien', true)),
+                'no_rm' => htmlspecialchars($this->input->post('no_rm', true)),
+                'nama_pasien' => htmlspecialchars($this->input->post('nama_pasien', true)),
+                'tgl_lahir' => htmlspecialchars($this->input->post('tgl_lahir', true)),
+                'alamat' => htmlspecialchars($this->input->post('alamat', true))
+            ]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Patient Data successfully added!</div>');
+            redirect('rekam_medis');
+        }
+    }
 }
--- application/views/daftar/dataPasien.php
+++ application/views/daftar/dataPasien.php
@@ -29,6 +29,7 @@
         <?= $this->session->flashdata('message'); ?>
         <div class="row">
           <div class="col-sm-12 col-md-6">
+            <button type="button" class="btn btn-outline-primary btn-lg mb-3 ml-3" data-toggle="modal" data-target="#newTambahPasienModal">Tambah Pasien</button>
           </div>
           <div class="col-sm-12 col-md-6">
             <div id="example2_filter" class="dataTables_filter">
@@ -40,7 +41,7 @@
         </div>
         <div class="card">
           <div class="card-body">
-            <table id="example2" class="table table-bordered table-hover text-center">
+            <table id="example2" class="table table-bordered table-hover table-sm text-center">
               <thead>
                 <tr class="table-active">
                   <th>No</th>
@@ -64,7 +65,7 @@
                     <td><?= $dpd['alamat']; ?></td>
                     <td>
                       <button type="button" class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-pen-to-square"></i> Entry</button>
-                      <button type="button" class="btn btn-outline-secondary btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-clock-rotate-left"></i> History</button>
+                      <button type="button" class="btn btn-outline-dark btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-trash-can"></i> History</button>
                       <button type="button" class="btn btn-outline-warning btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-print"></i> Print</button>
                       <button type="button" class="btn btn-outline-danger btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-file-pdf"></i> pdf</button>
                       <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#newSubMenuModal"><i class="fas fa-solid fa-file-excel"></i> excel</button>
@@ -90,44 +91,41 @@
 <!-- End of Main Content -->
 
 <!-- Modal -->
-<div class="modal fade" id="newSubMenuModal" tabindex="-1" aria-labelledby="newSubMenuModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade" id="newTambahPasienModal" tabindex="-1" aria-labelledby="TambahPasienModal" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="newSubMenuModalLabel">Add New Sub Menu</h5>
+        <h5 class="modal-title" id="TambahPasienModal">Tambah Data Pasien</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="<?= base_url('menu/submenu'); ?>" method="post">
+      <form action="<?= base_url('rekam_medis/create'); ?>" method="post">
         <div class="modal-body">
           <div class="form-group">
-            <input type="title" class="form-control" id="title" name='title' placeholder="Submenu title">
+            <label for="id_pasien">ID Pasien</label>
+            <input type="number" class="form-control form-control-sm" id="id_pasien" name='id_pasien'>
           </div>
           <div class="form-group">
-            <select name="menu_id" id="menu_id" class="form_control">
-              <option value="">Select Menu</option>
-              <?php foreach ($menu as $m) : ?>
-                <option value="<?= $m['id']; ?>"><?= $m['menu']; ?></option>
-              <?php endforeach; ?>
-            </select>
+            <label for="no_rm">No Rekam Medis</label>
+            <input type="number" class="form-control form-control-sm" id="no_rm" name='no_rm'>
           </div>
           <div class="form-group">
-            <input type="title" class="form-control" id="url" name='url' placeholder="Submenu url">
+            <label for="nama_pasien">Nama Pasien</label>
+            <input type="text" class="form-control form-control-sm" id="nama_pasien" name='nama_pasien'>
           </div>
           <div class="form-group">
-            <input type="title" class="form-control" id="icon" name='icon' placeholder="Submenu icon">
+            <label for="tgl_lahir">Tanggal Lahir</label>
+            <input type="date" class="form-control form-control-sm" id="tgl_lahir" name='tgl_lahir'>
           </div>
           <div class="form-group">
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" value="1" name='is_active' id="is_active" checked>
-              <label for="is_active" class="form-check-label">Active?</label>
-            </div>
+            <label for="alamat">Alamat</label>
+            <input type="text" class="form-control form-control-sm" id="alamat" name='alamat'>
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Add</button>
+          <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-outline-primary">Add</button>
         </div>
       </form>
     </div>
