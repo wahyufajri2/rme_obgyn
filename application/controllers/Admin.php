ad08d1253c67b7afb2ec81db42d04099fddb6206
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-03 07:55:20 +0700

    Memperbaiki tampilan tambah akun

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -135,6 +135,8 @@ class Admin extends CI_Controller
 
         if ($this->form_validation->run() == FALSE) {
             $data['title'] = 'Tambah Akun Baru';
+            $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
             $this->load->view('templates/topbar', $data);
