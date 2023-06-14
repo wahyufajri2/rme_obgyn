d8fe9383b1c0d291138bfe785a110c6be3a350a1
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-14 10:20:33 +0700

    Menambahkan modal update di master data pasien (rekam medis)2

--- application/views/pendaftaran/masterPasien.php
+++ application/views/pendaftaran/masterPasien.php
@@ -57,8 +57,8 @@
                       <td class="text-center"><?= $dpd['alamat']; ?></td>
                       <td>
                         <div class="text-center">
-                          <a href="" alt="Entri Data" class="btn btn-outline-primary btn-sm text-center" data-toggle="modal" data-target="#newModal"><i class="fas fa-solid fa-pen-to-square fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Entri</a>
-                          <a href="#" class="btn btn-outline-success btn-sm text-center" data-toggle="modal" data-target="#EditPasienModal<?php echo $dpd['id_pasien']; ?>"><i class="fas fa-solid fa-clock-rotate-left fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Riwayat</a>
+                          <a href="" alt="Entri Data" class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#newModal"><i class="fas fa-solid fa-pen-to-square fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Entri</a>
+                          <a href="#" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#EditPasienModal<?php echo $dpd['id_pasien']; ?>"><i class="fas fa-solid fa-clock-rotate-left fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Riwayat</a>
                           <a href="<?= base_url(); ?>pendaftaran/deleteMasterPasien/<?= $dpd['id_pasien']; ?>" class="btn btn-outline-danger btn-sm text-center" onclick="return confirm('Apakah yakin menghapus data dari <?php echo $dpd['nama_pasien'] ?>?');"><i class="fas fa-solid fa-trash-can fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Hapus</a>
                         </div>
                         <!-- Edit Modal -->
