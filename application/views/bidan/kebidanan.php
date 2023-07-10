ede0ef8b053b468cfc54bdb64ea53dd3ec23536e
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-07-11 03:15:17 +0700

    Menghilangkan fungsi create pada menu bidan dan perawat

--- application/views/bidan/kebidanan.php
+++ application/views/bidan/kebidanan.php
@@ -29,7 +29,7 @@
                 <?= $this->session->flashdata('message'); ?>
                 <div class="card shadow mb-4">
                     <div class="card-header">
-                        <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#TambahPasienAsesment"><i class="fas fa-solid fa-circle-plus fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Tambah Pasien</button>
+                        <!-- <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#TambahPasienAsesment"><i class="fas fa-solid fa-circle-plus fa-beat-fade" style="--fa-beat-fade-opacity: 0.67; --fa-beat-fade-scale: 1.075;"></i> Tambah Pasien</button> -->
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
