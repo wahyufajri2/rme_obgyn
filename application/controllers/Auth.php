d9b50e4bac094e975c69f6f5f9faaa62a50763b9
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2024-08-22 16:55:11 +0700

    Memperbarui controller auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -110,51 +110,6 @@ class Auth extends CI_Controller
         }
     }
 
-    public function registration()
-    {
-        if ($this->session->userdata('email')) {
-            redirect('user');
-        }
-
-        $this->form_validation->set_rules('nama', 'Name', 'required|trim');
-        $this->form_validation->set_rules(
-            'email',
-            'Email',
-            'required|trim|valid_email|is_unique[user.email]',
-            [
-                'is_unique' => 'Email ini sudah terdaftar!'
-            ]
-        );
-        $this->form_validation->set_rules('kata_sandi1', 'Password', 'required|trim|min_length[8]|matches[password2]', [
-            'matches' => 'Kata sandi tidak cocok!',
-            'min_length' => 'Kata sandi terlalu pendek!'
-        ]);
-        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]', [
-            'matches' => 'Kata sandi tidak cocok!',
-        ]);
-
-        if ($this->form_validation->run() == FALSE) {
-            $data['title'] = 'Registrasi Pengguna';
-            $this->load->view('templates/auth_header', $data);
-            $this->load->view('auth/registration');
-            $this->load->view('templates/auth_footer');
-        } else {
-            $data = [
-                'nama' => htmlspecialchars($this->input->post('nama', true)),
-                'email' => htmlspecialchars($this->input->post('email', true)),
-                'gambar' => 'default.jpg',
-                'kata_sandi' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-                'id_peran' => 2,
-                'apakah_aktif' => 1,
-                'tgl_dibuat' => time()
-            ];
-
-            $this->db->insert('pengguna', $data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Akun Anda telah dibuat. Silakan Login</div>');
-            redirect('auth');
-        }
-    }
-
     public function logout()
     {
         $this->session->unset_userdata('email');
