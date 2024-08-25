bfb12c7faeab3ddeb05207caa9e92f4e5cbde0d1
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2024-08-25 23:34:33 +0700

    Memperbaiki tampilan dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,7 +7,7 @@ class Dashboard extends CI_Controller
     {
         parent::__construct();
         if (!$this->session->userdata('email')) {
-            redirect('auth'); // Redirect jika belum login
+            redirect('auth'); // Redirect ke halaman login, jika belum login
         }
     }
 
@@ -16,6 +16,18 @@ class Dashboard extends CI_Controller
         $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('pengguna', ['email' => $this->session->userdata('email')])->row_array();
         $data['role'] = $this->db->get('peran_pengguna')->result_array();
+
+        // Ambil jumlah baris data dari tabel 'pendaftaran'
+        $this->db->from('pendaftaran'); // Ganti 'pendaftaran' dengan nama tabel Anda jika berbeda
+        $query = $this->db->get();
+        $data['jumlah_pendaftaran'] = $query->num_rows();
+
+        // Ambil jumlah pendaftaran per hari ini
+        $today = date('Y-m-d'); // Mendapatkan tanggal hari ini dalam format YYYY-MM-DD
+        $this->db->from('pendaftaran');
+        $this->db->where('DATE(tgl_pendaftaran)', $today); // Asumsikan kolom tanggal pendaftaran adalah 'tgl_pendaftaran'
+        $query_today = $this->db->get();
+        $data['jumlah_pendaftaran_hari_ini'] = $query_today->num_rows();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -13,9 +13,9 @@
                     <div class="card-body">
                         <h2 class="card-title">Jumlah pasien hari ini</h2>
                         <h5 class="card-text">
-                            <div class="card" style="width: 18rem;">
+                            <div class="card text-white" style="width: 18rem; background: linear-gradient(80deg, #13290d, #0abb01);">
                                 <div class="card-body">
-                                    20 Pasien
+                                    <?= $jumlah_pendaftaran_hari_ini; ?> Pasien
                                 </div>
                             </div>
                         </h5>
@@ -27,9 +27,9 @@
                     <div class="card-body">
                         <h2 class="card-title">Jumlah pasien keseluruhan</h2>
                         <h5 class="card-text">
-                            <div class="card" style="width: 18rem;">
+                            <div class="card text-white" style="width: 18rem; background: linear-gradient(80deg, #13290d, #0abb01);">
                                 <div class="card-body">
-                                    100 Pasien
+                                    <?= $jumlah_pendaftaran; ?> Pasien
                                 </div>
                             </div>
                         </h5>
@@ -42,13 +42,13 @@
                 <div class="card-body">
                     <h2 class="card-title text-center" style="font-weight: 700;">Selamat Datang Di <br>Rekam Medis Asesmen Obstetri dan Ginekologi <br>[RS PKU Muhammadiyah Gamping]</h2>
                     <hr>
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="<?= base_url('assets/'); ?>img/pku-gamping.jpg" class="img-fluid rounded-start" alt="RS PKU Gamping">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <p class="card-text" style="text-align: justify; font-size:16px;">Asesmen Obstetri dan Ginekologi adalah proses evaluasi komprehensif terhadap kesehatan reproduksi wanita, baik yang berkaitan dengan kehamilan (obstetri) maupun yang tidak berkaitan dengan kehamilan (ginekologi). Asesmen ini dilakukan oleh tenaga kesehatan profesional, seperti dokter spesialis kebidanan dan kandungan (obgyn) atau bidan.</p>
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="<?= base_url('assets/'); ?>img/pku-gamping.jpg" class="img-fluid rounded-start" alt="RS PKU Gamping">
+                            </div>
+                            <div class="col-md-8" style="background: linear-gradient(80deg, #13290d, #0abb01);">
+                                <p class="card-text text-white p-2 fw-semibold" style="text-align: justify; font-size:16px;">Asesmen Obstetri dan Ginekologi adalah proses evaluasi komprehensif terhadap kesehatan reproduksi wanita, baik yang berkaitan dengan kehamilan (obstetri) maupun yang tidak berkaitan dengan kehamilan (ginekologi). Asesmen ini dilakukan oleh tenaga kesehatan profesional, seperti dokter spesialis kebidanan dan kandungan (obgyn) atau bidan.</p>
                             </div>
                         </div>
                     </div>
